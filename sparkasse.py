2590c25c3d0763bcc87d3048d48b15f86af65781
Anton <git@maestr0.com>
2015-07-22 19:44:20 +0200

    Add class for Sparkasse parsing

---
+++ sparkasse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Run with
+./sparkasse.py
+"""
+
+import io
+import csv
+import sys
+import logging
+import time
+from decimal import Decimal
+
+from converter import Converter
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class Sparkasse(Converter):
+    """
+    Implementation for Sparkasse bank
+    """
+    def __init__(self, *args, **kwargs):
+        super(Sparkasse, self).__init__(*args, **kwargs)
+
+if __name__ == '__main__':
+    ynab_file = "ynab_data_sparkasse.csv"
+    spk_file = sys.argv[1]
+    converter = Sparkasse()
+    input_data = converter.load_transactions(spk_file)
+    ynab_data = []
+    for row in input_data:
+        ynab_data.append(converter.convert_row(row))
+    converter.export_file(ynab_file, ynab_data)
